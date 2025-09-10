29638464b6b8ad395c645607f7d3b8be517aed4d
Bugra Arslan <bugraarslan5@outlook.com>
2025-09-10 22:08:45 +0300

    Update app configuration and layout for improved functionality and structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,19 @@
+import 'react-native-gesture-handler';
 import { Stack } from 'expo-router/stack';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
     useFrameworkReady();
 
     return (
-        <>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="index" />
                 <Stack.Screen name="+not-found" />
             </Stack>
             <StatusBar style="auto" />
-        </>
+        </GestureHandlerRootView>
     );
 }
